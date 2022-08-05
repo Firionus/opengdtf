5f4d9d3d37d044414a4ddeaa90a040ebb8268de5
Johannes Fried <jcmf.schule@gmail.com>
2022-08-05 09:30:20 +0200

    Refactor main Function

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,15 @@ use opengdtf::Gdtf;
 use std::path::Path;
 
 fn main() {
-    println!("This parses a GDTF file and outputs the result the console");
+    println!("This parses a GDTF file and outputs the result to the console");
 
     let path =
         Path::new("test/resources/channel_layout_test/Test@Channel_Layout_Test@v1_first_try.gdtf");
+    
+    print_gdtf(path);
+}
+
+fn print_gdtf(path: &Path) {
     let gdtf = Gdtf::try_from(path).unwrap();
     println!("{:#?}", gdtf);
-
-    let path =
-        Path::new("this_really_shouldnt_exist");
-    let gdtf = Gdtf::try_from(path).unwrap_err();
-    println!("{:?}", gdtf);
-    println!("{}", gdtf);
-
-    let path =
-        Path::new("test/resources/channel_layout_test/Test@Channel_Layout_Test@v1_first_try (copy 1).gdtf");
-    let gdtf = Gdtf::try_from(path).unwrap_err();
-    println!("{:?}", gdtf);
-    println!("{}", gdtf);
-
-
 }
